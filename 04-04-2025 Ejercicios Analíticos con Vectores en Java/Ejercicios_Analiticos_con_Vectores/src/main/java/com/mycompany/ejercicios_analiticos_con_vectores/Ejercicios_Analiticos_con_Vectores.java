eb12175ec9f8956e40062d833177ba6a8449bdbb
Benigno Coj <bcojs@miumg.edu.gt>
2025-04-03 21:05:33 -0600

    Carga de ejercicio 04-04-2025

---
+++ 04-04-2025 Ejercicios Analíticos con Vectores en Java/Ejercicios_Analiticos_con_Vectores/src/main/java/com/mycompany/ejercicios_analiticos_con_vectores/Ejercicios_Analiticos_con_Vectores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicios_analiticos_con_vectores;
+
+/**
+ *
+ * @author brsan
+ */
+public class Ejercicios_Analiticos_con_Vectores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
